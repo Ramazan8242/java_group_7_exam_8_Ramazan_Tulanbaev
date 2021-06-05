56931583f5229b238cce968e8bd1ffed04b861a4
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-05 17:52:31 +0600

    finished task

--- static/script.js
+++ static/script.js
@@ -1,34 +1,65 @@
-class Country{
-    constructor(name,capital,flag,currencies,region , alpha2Code) {
-        this.name = name,
-            this.capital = capital,
-            this.flag = flag,
-            this.currencies = currencies,
-            this.region = region,
-            this.alpha2Code = alpha2Code
+class Country {
+    constructor(name, capital, flag, region, currency, alpha2Code) {
+        this.name = name;
+        this.capital = capital;
+        this.flag = flag;
+        this.currency = currency;
+        this.region = region;
+        this.alpha2Code = alpha2Code;
     }
 }
 
-let contr = []
-function addCountry(countries) {
-    let country = contr[i]
-    let createDiv = document.createElement('div')
-    createDiv.innerHTML=
-        `<h1>${countries.name}</h1>
-        <p>Capital: ${countries.capital}</p>
-        <p>${countries.flag}</p>
-        <p>Currencies: ${countries.currencies}</p>
-        <p>Region: ${countries.region}</p> 
-         <a href="https://www.google.com/search?q=${countries.name}" target="_blank">More</a>   `
- }
+let countries = [];
+function display(i) {
+    const country = countries[i];
+    let div = document.createElement('div');
+    div.classList.add('border', 'mt-2', 'w-75')
+    div.innerHTML = `<h1>${country.name}</h1><h3>Столица: ${country.capital}</h3><p><img src="${country.flag}"></p><p>Регион: ${country.currency}</p><p>Волюта: ${country.region}</p>
+                    <p>Ссылка на информацию об этой стране: <a href="https://www.google.com/search?q=${country.name}" target="_blank">Information</a></p>
+                    <p>Ссылка на вивипедию: <a href="https://ru.m.wikipedia.org/wiki/${country.name}" target="_blank"> Wikipedia</a></p>`;
+    document.getElementsByClassName('cities')[0].prepend(div);
+}
 
- async function getCont(countries) {
+
+async function getCountry() {
     await fetch('https://restcountries.eu/rest/v2/all')
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            for (let i = 0; i < 100; i++) {
-                contr.push(new Country(json[i].name,json[i].capital,json[i].flag,json[i].currencies,json[i].region))
+        .then(r => r.json()).then(j => {
+            for (let i = 0; i < 250; i++) {
+                countries.push(new Country(j[i].name, j[i].capital, j[i].flag, j[i].currencies[0].name, j[i].region, j[i].alpha2Code));
             }
-        })
- }
+        });
+}
+
+getCountry().then(r => {
+    Warnings();
+    console.log(r);
+});
+
+function Warnings() {
+    const submit = document.getElementsByClassName('submit')[0];
+    submit.addEventListener('click', function (event) {
+        const form = document.getElementsByClassName('inline')[0];
+        const form2 = form.elements;
+        if (form2['country-name-form'].value === "") {
+            alert("Пожалуйста, напишите название страны");
+        } else if (form2['country-name-form'].value.length < 2) {
+            alert("Пожалуйста, напишите название страны более чем на 2 символа");
+        } else {
+            сharacters(form2['country-name-form'].value);
+        }
+        form2['country-name-form'].value = '';
+        form2['country-name-form'].focus();
+        event.preventDefault();
+    });
+}
+
+function сharacters(t) {
+    let e = t;
+    let f = false;
+    for (let i = 0; i < countries.length; i++) {
+        if (e.length > 0) {
+            if (countries[i].name === e) {display(i);f = true;}
+        }if (e.length === 2) {
+            if (countries[i].alpha2Code === e) {display(i);f = true;}
+        }}if (!f) {alert("Мы не нашли такой страны.");}
+}
