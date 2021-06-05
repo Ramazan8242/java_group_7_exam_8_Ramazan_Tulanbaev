c7ba37706902563e54c5d59f1001378426d728e5
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-05 15:14:15 +0600

    Added a constructor

---
+++ static/script.js
@@ -0,0 +1,11 @@
+class Country{
+    constructor(name,capital,flag,currencies,region , alpha2Code) {
+        this.name = name,
+            this.capital = capital,
+            this.flag = flag,
+            this.currencies = currencies,
+            this.region = region,
+            this.alpha2Code = alpha2Code
+    }
+}
+Added a constructor
