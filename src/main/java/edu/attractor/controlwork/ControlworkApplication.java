78298051fe3e6576841c34aa39250d9f1de8e2aa
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-05 11:33:25 +0600

    initial commit

---
+++ src/main/java/edu/attractor/controlwork/ControlworkApplication.java
@@ -0,0 +1,13 @@
+package edu.attractor.controlwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/attractor/controlwork/ControlworkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.attractor.controlwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
