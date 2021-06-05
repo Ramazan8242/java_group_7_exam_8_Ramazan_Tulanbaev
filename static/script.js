22bdaeea95c9d7fe411ad5f047e47c527942f120
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-05 15:16:28 +0600

    added method getCont

--- static/script.js
+++ static/script.js
@@ -21,3 +21,14 @@ function addCountry(countries) {
         <p>Region: ${countries.region}</p> 
          <a href="https://www.google.com/search?q=${countries.name}" target="_blank">More</a>   `
  }
+
+ async function getCont(countries) {
+    await fetch('https://restcountries.eu/rest/v2/all')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            for (let i = 0; i < 100; i++) {
+                contr.push(new Country(json[i].name,json[i].capital,json[i].flag,json[i].currencies,json[i].region))
+            }
+        })
+ }
